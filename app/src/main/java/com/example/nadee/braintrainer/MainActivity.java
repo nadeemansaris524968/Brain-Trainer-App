67ec426f28e1d65b3856511c4bbf5869fd471465
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-05-07 20:11:18 -0500

    Revamped UI, change in logic for displaying questions and answers

--- app/src/main/java/com/example/nadee/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/nadee/braintrainer/MainActivity.java
@@ -8,15 +8,69 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startBTN;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    TextView sumTextView;
+    ArrayList<Integer> answers = new ArrayList<>();
+    int locationOfCorrectAnswer;
+
+    public void start(View view){
+        startBTN.setVisibility(View.INVISIBLE);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBTN = (Button) findViewById(R.id.startBTN);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        sumTextView = (TextView)findViewById(R.id.sumTextView);
+
+        Random random = new Random();
+        int a = random.nextInt(21);
+        int b = random.nextInt(21);
+
+        sumTextView.setText(a+" + "+b);
+
+        locationOfCorrectAnswer = random.nextInt(4);
+        int incorrectAnswer;
+
+        for (int i=0;i<4;i++){
+            if (i == locationOfCorrectAnswer) {
+                answers.add(a + b);
+            }
+            else {
+                incorrectAnswer = random.nextInt(41);
+                while (incorrectAnswer == a+b){
+                    incorrectAnswer = random.nextInt(41);
+                }
+                answers.add(incorrectAnswer);
+            }
+        }
+
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+
+    }
+
+    public void chooseAnswer(View view){
+        
     }
 }
